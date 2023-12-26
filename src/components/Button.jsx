0add04c50e34aaf5e40eab616e4988accf5b1beb
Faruqoloyede <oloyedefaruq2@gmail.com>
2023-12-25 18:16:21 -0800

    added google form

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props, index) => {
   const styles = props.styles ?? ""
 
   return (
-    <button style={{color: props.color}} className={`py-2 px-6 font-medium font-poppins text-[18px] rounded-[10px] ${styles} btn ${index == Button.length -1 ? "mr-0" : "mr-10"} mb-3`}>{label}</button>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSd9mnXLd9AkP3T3lWFd_Zekh_EQ5anIVZz_abu0To52qRTxrg/viewform?usp=sf_link" target="_blank"  style={{color: props.color}} className={`py-2 px-6 font-medium font-poppins text-[18px] rounded-[10px] ${styles} btn ${index == Button.length -1 ? "mr-0" : "mr-10"} mb-3`} >{label}</a>
   )
 }
 
