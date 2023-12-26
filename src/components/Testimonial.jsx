e8b206690cad453294675f2c8fa2407e727168fb
Faruqoloyede <oloyedefaruq2@gmail.com>
2023-12-25 17:36:02 -0800

    aded getstarted section

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,12 +1,14 @@
 import money from "../assets/money.png";
+import Button from "./Button";
 
 const Testimonial = () => {
   return (
     <section>
       <div className='flex md:flex-row flex-col items-center justify-center'>
-        <div className='flex flex-col'>
-          <h2 className='font-space text-[30px] font-bold mb-4'>Urgent Tukay- the Notes</h2>
-          <p className='font-space font-normal text-[18px]'>No cash, No wahala get our application and deal with Sapa.</p>
+        <div className='flex flex-col items-start'>
+          <h2 className='font-space text-[30px] font-bold mb-4 md:mt-0 mt-8'>Urgent Tukay- the Notes</h2>
+          <p className='font-space font-normal text-[18px] my-3'>No cash, No wahala get our application and deal with Sapa.</p>
+          <Button styles="bg-primary" color= "white" />
         </div>
         <img src={money} />
       </div>
